bc698f7b04db83db4d0f9f86f9a350470d5ab00d
pmlabiiserT <16pmlab@gmail.com>
2026-05-29 00:59:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,69 +23,6 @@ export default function Home() {
 
   <div className="absolute inset-0 bg-black/25 z-0" />
 
-  {/* ================================================= */}
-  {/* HOVER MENU */}
-  {/* ================================================= */}
-
-  <div className="fixed top-10 left-10 z-[99999] group">
-
-    {/* MENU ICON */}
-
-    <div className="w-16 h-16 rounded-2xl backdrop-blur-xl bg-black/75 border border-cyan-400/30 flex flex-col justify-center items-center gap-2 cursor-pointer shadow-[0_0_35px_rgba(34,211,238,0.25)] hover:scale-110 transition-all duration-300">
-
-      <div className="w-8 h-[3px] bg-cyan-300 rounded-full" />
-      <div className="w-8 h-[3px] bg-cyan-300 rounded-full" />
-      <div className="w-8 h-[3px] bg-cyan-300 rounded-full" />
-
-    </div>
-
-    {/* EXPANDING MENU */}
-
-    <div className="absolute top-0 left-0 w-0 opacity-0 overflow-hidden group-hover:w-[420px] group-hover:opacity-100 transition-all duration-500">
-
-      <div className="ml-24 min-w-[300px] backdrop-blur-2xl bg-black/92 border border-cyan-400/30 rounded-[35px] p-10 shadow-[0_0_80px_rgba(34,211,238,0.4)]">
-
-        <div className="flex flex-col gap-7 text-2xl">
-
-          <Link href="/" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            Home
-          </Link>
-
-          <Link href="/about" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            Team Leader
-          </Link>
-
-          <Link href="/research" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            Research
-          </Link>
-
-          <Link href="/publications" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            Publications
-          </Link>
-
-          <Link href="/facilities" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            Research Facilities
-          </Link>
-
-          <Link href="/team" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            Team
-          </Link>
-
-          <Link href="/news" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            News & Gallery
-          </Link>
-
-          <Link href="/contact" className="text-cyan-300 hover:text-white hover:translate-x-3 transition-all duration-300">
-            Opportunities & Contact
-          </Link>
-
-        </div>
-
-      </div>
-
-    </div>
-
-  </div>
 
 {/* ================================================= */}
   {/* TOP LOGOS */}
