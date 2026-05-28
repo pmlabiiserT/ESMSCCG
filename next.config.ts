f28867289d5ba3fb33d8d8c94d31658e22a22dc3
pmlabiiserT <16pmlab@gmail.com>
2026-05-28 23:32:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/ESMSCCG-main',
+  basePath: '/ESMSCCG',
   images: {
     unoptimized: true,
   },
