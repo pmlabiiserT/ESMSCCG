153c2fc0069be0825531ff62c4dc2f916b69093e
pmlabiiserT <16pmlab@gmail.com>
2026-09-17 23:07:16 +0530

    Update page.tsx

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -47,7 +47,7 @@ export default function ResearchPage() {
 
 {/* Enter Research Sections */}
 
-<div className="w-full flex justify-center -mt-0 mb-10">
+<div className="w-full flex justify-center -mt-5 mb-10">
 
   <a
     href="#research-start"
